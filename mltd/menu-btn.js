249846648f0e76e6f5fb45958faa47405fa47c5a
Violet-K07 <80018055+Violet-K07@users.noreply.github.com>
2021-05-18 10:54:51 +0800

    mltd

---
+++ mltd/menu-btn.js
@@ -0,0 +1,24 @@
+$('#Profile-btn').click(
+	function(){
+		$('html,body').animate(
+			{scrollTop:$('#Profile').offset().top
+			}, 200);
+	});
+$('#Idol-btn').click(
+	function(){
+		$('html,body').animate(
+			{scrollTop:$('#Idol').offset().top
+			}, 400);
+	});
+$('#Scene-btn').click(
+	function(){
+		$('html,body').animate(
+			{scrollTop:$('#Scene').offset().top
+			}, 400);
+	});
+$('#MV-btn').click(
+	function(){
+		$('html,body').animate(
+			{scrollTop:$('#MV').offset().top
+			}, 400);
+	});
---
+++ mltd/page-top.js
@@ -0,0 +1,16 @@
+document.getElementById("page-top").style.display = "none";
+window.onscroll = function () { 
+	if (document.documentElement.scrollTop + document.body.scrollTop > 100)/*鼠标滚动距离*/ { 
+		document.getElementById("page-top").style.display = "block"; /*大于显示*/
+	} 
+	else { 
+		document.getElementById("page-top").style.display = "none"; /*小于隐藏*/
+	} 
+} 
+$(function(){
+	$("#page-top").click(function(){ 
+		$("html,body").animate({
+			scrollTop:"0px"
+		},400);
+	});  
+});
